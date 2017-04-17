bb7a4ad8668f7f0725edd98548433ef2b9645292
Eric Lobdell <eric@ericlobdell.me>
2017-04-16 20:41:59 -0400

    initial commit

---
+++ Runner/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using static System.Console;
+
+namespace Runner
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var controllers = GetControllerTypes<ApiTestGen.MvcApplication>();
+
+      foreach (Type controller in controllers)
+      {
+        var info = GetControllerInfo(controller);
+        WriteLine($"{info.Name}");
+        
+        foreach (var method in info.Methods)
+        {
+          var methodParams = method
+            .GetParameters()
+            .Select(a => $"{a.ParameterType} {a.Name}");
+
+          WriteLine($"\t{method.Name}({string.Join(",", methodParams)}) -> {method.ReturnType}");
+        }
+
+        WriteLine("\n------------------------------------\n");
+      }
+
+      Read();
+    }
+
+    static IEnumerable<Type> GetControllerTypes<T>()
+    {
+      return typeof(T).Assembly.GetTypes().Where(ex =>
+        ex.BaseType == typeof(Controller) ||
+        ex.BaseType == typeof(ApiController));
+    }
+
+    static ControllerInfo GetControllerInfo(Type controller)
+    {
+      var controllerType = typeof(Controller);
+      return new ControllerInfo
+      {
+        Type = controllerType,
+        Name = controllerType.Name,
+        Methods = controllerType
+          .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+      };
+    }
+  }
+
+  public class ControllerInfo
+  {
+    public string Name { get; set; }
+    public Type Type { get; set; }
+    public IEnumerable<MethodInfo> Methods { get; set; }
+  }
+}
